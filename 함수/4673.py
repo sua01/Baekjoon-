a631abb334a356ace99bed9eaea702672e2491d8
sua01 <sua0211@naver.com>
2022-01-25 21:20:21 +0900

    파이썬은 아직 안됨

--- 함수/4673.py
+++ 함수/4673.py
@@ -1,27 +1,29 @@
 #22.01.25
 #백준 함수 - 4673번 : 셀프 넘버
 
-list = [0 for i in range(1000)]
+
+list = [0 for i in range(10000)]
 x=1
 
 while True:
-    i = 1
     res = x
-    a = b = x
+    a = x
 
     if(x > 1000):
         break
 
-    while b > 0 :
-        a = b % (10*i)
-        b = x // (10*i)
-        i*=10
-        res+=a
-    print(res)
-    list[res-1] = 1
-    x+=1
+    while a > 0 :
+        res += a%10
+        a = a // 10
+   
+    if res<=10000:
+        list[res-1] = 1
 
-for i in range(0, len(list)-1):
+    x+=1
+    if x>10000:
+        break
+
+for i in range(0, 10000):
     if list[i] != 1:
         print(i+1)
    
