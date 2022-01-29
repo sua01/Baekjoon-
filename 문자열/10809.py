d17b9e448c3d9548f113242b45e022ded2cd9650
sua01 <sua0211@naver.com>
2022-01-29 22:43:51 +0900

    1-29

--- 문자열/10809.py
+++ 문자열/10809.py
@@ -1,1 +1,12 @@
 #백준 10809 : 알파벳 찾기
+
+alph = [-1 for i in range(26)]
+c = input()
+
+for i in range(len(c)):
+    for j in range(len(alph)):
+        if ord(c[i])-97==j and alph[j]==-1:
+            alph[j] = i
+
+for i in range(len(alph)):
+    print(alph[i], end=' ')
