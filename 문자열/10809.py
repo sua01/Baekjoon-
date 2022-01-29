fcd49d160dc768b1b498f00f87bbf310c27b8be9
sua01 <sua0211@naver.com>
2022-01-29 22:44:44 +0900

    1-29

--- 문자열/10809.py
+++ 문자열/10809.py
@@ -5,7 +5,7 @@ c = input()
 
 for i in range(len(c)):
     for j in range(len(alph)):
-        if ord(c[i])-97==j and alph[j]==-1:
+        if ord(c[i])-97==j and alph[j]==-1: #아스키코드 이용해서 구분, 중복된 문자는 첫 숫서로
             alph[j] = i
 
 for i in range(len(alph)):
