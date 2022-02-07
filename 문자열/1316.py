3010fe985cb4f9fc0126bb744ca3873462681e51
sua01 <sua0211@naver.com>
2022-02-07 23:55:42 +0900

    2-7

--- 문자열/1316.py
+++ 문자열/1316.py
@@ -1,5 +1,12 @@
 #백준 1316 : 그룹 단어 체커
 
 num = int(input()) # 단어 개수 입력받기
-for i in range(num):
+
+res = 0
+for _ in range(num):
     word = input()
+    for i in range(len(word)-1):
+        if word[i] != word[i+1]:
+
+
+print(res)
