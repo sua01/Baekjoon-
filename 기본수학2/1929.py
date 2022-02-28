909608ec3966ffa45603cef2d0c6e2c15840d8ca
sua01 <sua0211@naver.com>
2022-02-28 21:19:45 +0900

    2-28

--- 기본수학2/1929.py
+++ 기본수학2/1929.py
@@ -1,4 +1,17 @@
 # 백준 재귀 : 1929번 - 소수 구하기
+import math
 
+m, n = map(int, input().split())
 
+for i in range(m, n+1):
+    #if i ==1 : continue # 1은 소수가 아님
 
+    k = math.sqrt(i)    # i**0.5로 제곱근 구하기 가능
+
+    for j in range(2, int(k)+1):
+        if i % j == 0:  #소수가 아닌 경우
+            break
+
+    #else : print(i)  # 소수인 경우
+
+    
