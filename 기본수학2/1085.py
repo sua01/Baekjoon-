95407404882a0a07bbfed5660b182368cd6acbd3
sua01 <sua0211@naver.com>
2022-03-03 11:55:45 +0900

    3-3

---
+++ 기본수학2/1085.py
@@ -0,0 +1,11 @@
+# 백준 재귀 : 1085번 - 직사각형에서 탈출
+
+x, y, w, h = map(int, input().split())
+
+'''min = 1000
+min=h-y if h-y < min else min
+min=w-x if w-x < min else min
+min=y if y < min else min
+min=x if x < min else min'''
+
+print(min(x, y, h-y, w-x))
